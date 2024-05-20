5ffece2e8fca0ca85c193980440a5fc4f2ec800d
Igors <166141617+CBuPenblu@users.noreply.github.com>
2024-05-20 23:32:29 +0300

    add family tree lesson

--- main2.py
+++ main2.py
@@ -10,7 +10,7 @@ class Bird():
         print(f"{self.name} eating")
 
     def sing(self):
-        print(f"{self.name} singing - {self.voice}")
+        print(f"{self.name} singing - coo")
 
     def info(self):
         print(f"{self.name} - name")
@@ -18,9 +18,20 @@ class Bird():
         print(f"{self.color} - bird color")
 
 class Pidgeon(Bird):
-    pass
+    def __init__(self, name, voice, color, favorite_food):
+        super().__init__(name, voice, color)
+        self.favorite_food = favorite_food
 
-bird1 = Pidgeon("CBuPenblu", "cooing", "grey")
+    def sing(self):
+        print(f"{self.name} sing - cooing cooing")
+
+    def walk(self):
+        print(f"{self.name} walking")
+
+bird1 = Pidgeon("CBuPenblu", "cooing", "grey", "seeds")
+
+bird2 = Bird("MAKAPOHbl", "coo", "brown")
 
 bird1.sing()
 bird1.info()
+bird1.walk()
